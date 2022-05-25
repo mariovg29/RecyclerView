53f8c8f5e0099dee25351759c75394e54460c472
Íkaro Chacal Guerrero <ikarochacalguerrero@gmail.com>
2022-05-25 06:20:35 -0500

    DataBinding habilitado

--- app/src/main/java/com/mariovaladez/recyclerview/Adapter/Lista_Basica_ViewHolder.kt
+++ app/src/main/java/com/mariovaladez/recyclerview/Adapter/Lista_Basica_ViewHolder.kt
@@ -8,21 +8,21 @@ import com.bumptech.glide.Glide
 import com.mariovaladez.recyclerview.Lista_Basica_DataClass
 import com.mariovaladez.recyclerview.Provider.Lista_Basica_Provider
 import com.mariovaladez.recyclerview.R
+import com.mariovaladez.recyclerview.databinding.ListaBasicaItemBinding
 
 class Lista_Basica_ViewHolder( view:View):RecyclerView.ViewHolder(view) {
 
+    val binding = ListaBasicaItemBinding.bind(view)
 
-    val foto     = view.findViewById<ImageView>(R.id.iv_lista_basica_foto)
-    val nombre   = view.findViewById<TextView>(R.id.tv_lista_basica_nombre)
-    val apellido = view.findViewById<TextView>(R.id.tv_lista_basica_apellido)
-    val id       = view.findViewById<TextView>(R.id.tv_lista_basica_id)
+
+
 
     fun render(lista_basica_model:Lista_Basica_DataClass){
 
-        nombre.text = lista_basica_model.Nombre
-        apellido.text = lista_basica_model.Apellido
-        id.text = lista_basica_model.id.toString()
-        Glide.with(foto.context).load(lista_basica_model.foto).into(foto)
+        binding.tvListaBasicaNombre.text = lista_basica_model.Nombre
+        binding.tvListaBasicaApellido.text = lista_basica_model.Apellido
+        binding.tvListaBasicaId.text = lista_basica_model.id.toString()
+        Glide.with(binding.ivListaBasicaFoto.context).load(lista_basica_model.foto).into(binding.ivListaBasicaFoto)
 
 
 
