5aa8f39ac610cf225a10ef408a2c21b95027e281
Íkaro Chacal Guerrero <ikarochacalguerrero@gmail.com>
2022-05-25 15:13:25 -0500

    setOnClickListener en item de la funcion render

--- app/src/main/java/com/mariovaladez/recyclerview/Adapter/Lista_Basica_ViewHolder.kt
+++ app/src/main/java/com/mariovaladez/recyclerview/Adapter/Lista_Basica_ViewHolder.kt
@@ -3,6 +3,7 @@ package com.mariovaladez.recyclerview.Adapter
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.mariovaladez.recyclerview.Lista_Basica_DataClass
@@ -14,21 +15,29 @@ class Lista_Basica_ViewHolder( view:View):RecyclerView.ViewHolder(view) {
 
     val binding = ListaBasicaItemBinding.bind(view)
 
-
-
-
     fun render(lista_basica_model:Lista_Basica_DataClass){
 
         binding.tvListaBasicaNombre.text = lista_basica_model.Nombre
         binding.tvListaBasicaApellido.text = lista_basica_model.Apellido
         binding.tvListaBasicaId.text = lista_basica_model.id.toString()
-        Glide.with(binding.ivListaBasicaFoto.context).load(lista_basica_model.foto).into(binding.ivListaBasicaFoto)
+        Glide.with(binding.ivListaBasicaFoto.context)
+            .load(lista_basica_model.foto)
+            .into(binding.ivListaBasicaFoto)
 
+        binding.ivListaBasicaFoto.setOnClickListener { Toast.makeText(
+            binding.ivListaBasicaFoto.context,
+            lista_basica_model.Nombre+" "+lista_basica_model.id,
+            Toast.LENGTH_SHORT
+        ).show()
+        }
 
-
-
-
-
+        itemView.setOnClickListener {
+            Toast.makeText(
+                binding.ivListaBasicaFoto.context,
+                lista_basica_model.Apellido,
+                Toast.LENGTH_SHORT
+            ).show()
+        }
 
     }
 }
